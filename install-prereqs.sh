1ba2821cf8a9357b476ee57abfcf888bd95bb67c
nickrogal <51394648+nickrogal@users.noreply.github.com>
2021-03-31 22:02:16 -0400

    create additional files

--- install-prereqs.sh
+++ install-prereqs.sh
@@ -3,5 +3,6 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install postgresql postgresql-contrib -y
+sudo apt install phpmyadmin php-mbstring php-zip php-gd php-json php-curl -y
 sudo apt-get install python3-pip -y
 python3 -m pip install mysql-connector-python
