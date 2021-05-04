0a16b9b1e75ba07b12a4d45dcc20d167c7b038ae
nickrogal <51394648+nickrogal@users.noreply.github.com>
2021-05-03 21:01:44 -0400

    fuggetaboutit

--- install-prereqs.sh
+++ install-prereqs.sh
@@ -2,9 +2,9 @@
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
-#sudo apt-get install postgresql postgresql-contrib -y
-sudo apt install mysql-server -y
-sudo apt install phpmyadmin php-mbstring php-zip php-gd php-json php-curl -y
+sudo apt-get install postgresql postgresql-contrib -y
+#sudo apt install mysql-server -y
+#sudo apt install phpmyadmin php-mbstring php-zip php-gd php-json php-curl -y
 sudo apt-get install python3-pip -y
-python -m pip install mysql-connector-python
+#python -m pip install mysql-connector-python
 echo "Prerequsites installed, my guy!"
