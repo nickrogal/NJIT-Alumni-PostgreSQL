db9a78867b62b471b7c0b1dd10be6aae2c0c1107
nickrogal <51394648+nickrogal@users.noreply.github.com>
2021-05-05 23:46:43 -0400

    finished

--- populate-db.py
+++ populate-db.py
@@ -7,12 +7,16 @@ print("Database opened")
 cur = con.cursor()
 
 # Adds alumni into alumni table
+
+#Add Cheech Chong (no relation to Cheech & Chong)
 cur.execute('''INSERT INTO ALUMNI (ID, Name, Email , Major , College)
       VALUES (420, 'Cheech Chong', 'cc420@njit.edu', 'Computer Science', 'Ying Wu College of Computing');''')
 
+#Add Human Person. Definitely isn't a robot.
 cur.execute('''INSERT INTO ALUMNI (ID, Name, Email , Major , College)
       VALUES (69, 'Human Person', 'hh69@njit.edu', 'Information Technology', 'Ying Wu College of Computing');''')
 
+#Fuggetaboutit.
 cur.execute('''INSERT INTO ALUMNI (ID, Name, Email , Major , College)
       VALUES (86, 'Tony Soprano', 'ts86@njit.edu', 'Environmental Services and Waste Management', 'Newark College of Engineering');''')
 
